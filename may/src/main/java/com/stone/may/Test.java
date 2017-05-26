09c7b201e2c49bacaef2e7fb6c6a664dd61bbb80
chenchaohai <979839467@qq.com>
2017-05-27 00:05:06 +0800

    init commit

---
+++ may/src/main/java/com/stone/may/Test.java
@@ -0,0 +1,10 @@
+package com.stone.may;
+
+/**
+ * Created by chenchen on 2017/5/27.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("I want to change the world!");
+    }
+}
