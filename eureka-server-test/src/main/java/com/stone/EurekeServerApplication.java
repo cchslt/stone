e6ec681ee55ac3faab2427df54bef3a15e6262e5
chenchaohai <979839467@qq.com>
2017-06-18 15:50:15 +0800

    spring-cloud eureka

---
+++ eureka-server-test/src/main/java/com/stone/EurekeServerApplication.java
@@ -0,0 +1,15 @@
+package com.stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chenchen on 2017/6/17.
+ */
+@SpringBootApplication
+public class EurekeServerApplication {
+    public static void main(String[] args) {
+        //todo 未成功 —_—
+        SpringApplication.run(EurekeServerApplication.class);
+    }
+}
