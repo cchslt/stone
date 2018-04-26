ecf2a97473110fb63f33ccf8c8b1e8bb53f29ca0
shi <979839467@qq.com>
2018-04-26 23:13:36 +0800

    hello

--- pattern/src/main/java/com/design/pattern/proxy/Client.java
+++ pattern/src/main/java/com/design/pattern/proxy/Client.java
@@ -4,4 +4,7 @@ package com.design.pattern.proxy;
  * Created by chenchen on 2017/5/27.
  */
 public class Client {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
